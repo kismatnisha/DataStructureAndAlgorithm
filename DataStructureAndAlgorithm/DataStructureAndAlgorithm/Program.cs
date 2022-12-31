28f9721f697b984b1da7e2f40023470931130d49
qismat <qismatnisha6243@gmail.com>
2022-12-31 21:28:44 +0530

    [qismat] Add : UC7-StringReplace

--- DataStructureAndAlgorithm/DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select Programs\n 1.Binary search\n 2.Bubble sort\n 3.Anagram Detection\n 4.Check Prime Number\n 5.Check Number is Prime,palindrome or Anagram\n 6.Order List using Generics");
+                Console.WriteLine("Select Programs\n 1.Binary search\n 2.Bubble sort\n 3.Anagram Detection\n 4.Check Prime Number\n 5.Check Number is Prime,palindrome or Anagram\n 6.Order List using Generics\n 7.String repalce");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -36,6 +36,12 @@
                     case 6:
                         OrderList orderList = new OrderList();
                         orderList.Link(@"C:\Users\Suraj Sinha\OneDrive\Desktop\Bridgelabz\DataStructureAndAlgorithm\DataStructureandAlgorithm\Words.txt");
+                        break;
+                    case 7:
+                        StringReplace stringReplace = new StringReplace();
+                        stringReplace.Validate("\nHello << name >>, We have your fullname as << full name >> in our system.", "abc");
+                        stringReplace.ValidateNumber("your contact number is << contactno >>.", "91 7558570105");
+                        stringReplace.ValidateDate("Thank you BridgeLabz << Date >>.\n", "25/09/2022");
                         break;
                 }
             }
