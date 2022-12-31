deb21430f25741f91d2b8555f49dfc3f88a1ed64
qismat <qismatnisha6243@gmail.com>
2022-12-31 20:24:27 +0530

    [qismat] Add : UC1-BinarySearch

---
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructureAndAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Binary search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\Users\HP\Desktop\rfp\brigdelabz assignment\asignment\DataStructureAndAlgorithm\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                        break;
+                }
+            }
+        }
+    }
+}
