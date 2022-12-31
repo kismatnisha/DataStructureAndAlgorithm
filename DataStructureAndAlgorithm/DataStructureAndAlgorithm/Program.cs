23727f52127e236169dad9ee83b760ce0c02d9e7
qismat <qismatnisha6243@gmail.com>
2022-12-31 20:55:23 +0530

    [qismat] Add : UC5-PrimePalindromeAnagram

--- DataStructureAndAlgorithm/DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/Program.cs
@@ -7,17 +7,17 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select Programs\n 1.Binary search\n 2.Bubble sort\n 3.Anagram Detection\n 4.Check Prime Number");
+                Console.WriteLine("Select Programs\n 1.Binary search\n 2.Bubble sort\n 3.Anagram Detection\n 4.Check Prime Number\n 5.Check Number is Prime,palindrome or Anagram");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         BinarySearch binarysearch = new BinarySearch();
-                        binarysearch.Search(@"C:\Users\HP\Desktop\rfp\brigdelabz assignment\asignment\DataStructureAndAlgorithm\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                        binarysearch.Search(@"C:\Users\Suraj Sinha\OneDrive\Desktop\Bridgelabz\DataStructureAndAlgorithm\DataStructureandAlgorithm\Words.txt");
                         break;
                     case 2:
                         BubbleSort bubblesort = new BubbleSort();
-                        bubblesort.Sort(@"C:\Users\HP\Desktop\rfp\brigdelabz assignment\asignment\DataStructureAndAlgorithm\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                        bubblesort.Sort(@"C:\Users\Suraj Sinha\OneDrive\Desktop\Bridgelabz\DataStructureAndAlgorithm\DataStructureandAlgorithm\Words.txt");
                         break;
                     case 3:
                         Anagram anagram = new Anagram();
@@ -26,6 +26,12 @@
                     case 4:
                         PrimeNumber prime = new PrimeNumber();
                         prime.Prime();
+                        break;
+                    case 5:
+                        PrimePalindromeAnagram primePalimdrome = new PrimePalindromeAnagram();
+                        primePalimdrome.PrimeNumber();
+                        primePalimdrome.Palindrome();
+                        primePalimdrome.AnagramCheck();
                         break;
                 }
             }
